41684e4519cefcd8f3cd8f05458e713de4e431a3
Omar Ezzat <omar.ezzat@email.com>
2026-03-06 15:14:48 +0100

    feat: Implement canvas resizing, an animation loop, and a window resize event listener for the canvas.

--- apps/frontend/src/App.tsx
+++ apps/frontend/src/App.tsx
@@ -3,6 +3,17 @@ import { onMount } from "solid-js";
 
 function App() {
   let canvasRef!: HTMLCanvasElement
+  const resize = (width: number, height: number) => {
+    canvasRef.width = width * window.devicePixelRatio;
+    canvasRef.height = height * window.devicePixelRatio;
+    canvasRef.style.width = `${width}px`;
+    canvasRef.style.height = `${height}px`;
+    canvasRef.style.outline = 'none';
+    canvasRef.style.padding = '0px';
+    canvasRef.style.margin = '0px';
+  };
+  resize(window.innerWidth, window.innerHeight);
+
   onMount(async () => {
     const canvas = await new Canvas({
       canvas: canvasRef,
@@ -10,7 +21,21 @@ function App() {
     }).initialized
     console.log(canvas)
 
+    const animate = () => {
+      canvas.render();
+      requestAnimationFrame(animate);
+    };
+    animate();
+
+    window.addEventListener('resize', () => {
+      resize(window.innerWidth, window.innerHeight);
+      canvas.resize(window.innerWidth, window.innerHeight);
+    });
   })
+
+
+
+
   return (
     <div>
       <p>Vibecanvas Frontend is running.</p>
