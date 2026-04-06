fe4cc4af99ddf84637cc2dc1804bbdf624b66638
Omar Ezzat <omar.ezzat@email.com>
2026-04-06 13:56:31 +0200

    remove legacy packages shell and core

--- packages/imperative-shell/database-migrations/0005_puzzling_jubilee.sql
+++
@@ -1,1 +0,0 @@
-ALTER TABLE `agent_logs` DROP COLUMN `type`;
--- packages/imperative-shell/database-migrations/0008_rainy_puppet_master.sql
+++
@@ -1,2 +0,0 @@
-DROP INDEX IF EXISTS `files_hash_unique`;--> statement-breakpoint
-CREATE INDEX IF NOT EXISTS `files_hash_idx` ON `files` (`hash`);
