bec42a8bb5f5c0bbddb03f483eb251d88b56acf0
Omar Ezzat <omar.ezzat@email.com>
2026-04-18 17:03:59 +0200

    improve curry to not lose type info

--- packages/shared-functions/src/functional/fn.curry.ts
+++ packages/shared-functions/src/functional/fn.curry.ts
@@ -1,13 +1,33 @@
-export function fnCurry<TArgs extends unknown[], TResult>(
-  func: (...args: TArgs) => TResult,
-) {
-  return function fnCurried(this: unknown, ...args: unknown[]) {
+type TCurriedReturn<
+  TThis,
+  TArgs extends unknown[],
+  TResult,
+  TProvided extends unknown[],
+> = TArgs extends [...TProvided, ...infer TRest]
+  ? TRest extends []
+    ? TResult
+    : TCurried<TThis, TRest, TResult>
+  : never;
+
+type TCurried<TThis, TArgs extends unknown[], TResult> = <
+  TProvided extends unknown[],
+>(
+  this: TThis,
+  ...args: TArgs extends [...TProvided, ...infer _] ? TProvided : never
+) => TCurriedReturn<TThis, TArgs, TResult, TProvided>;
+
+export function fnCurry<TThis, TArgs extends unknown[], TResult>(
+  func: (this: TThis, ...args: TArgs) => TResult,
+): TCurried<TThis, TArgs, TResult> {
+  function fnCurried(this: TThis, ...args: unknown[]) {
     if (args.length >= func.length) {
-      return func.apply(this, args as unknown as TArgs);
+      return func.apply(this, args as TArgs);
     }
 
-    return function (this: unknown, ...args2: unknown[]) {
+    return function (this: TThis, ...args2: unknown[]) {
       return fnCurried.apply(this, args.concat(args2));
     };
-  };
+  }
+
+  return fnCurried as TCurried<TThis, TArgs, TResult>;
 }
